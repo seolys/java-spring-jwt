3a73aa3dee34339b02bb237993cc56f766385864
설연수 <seolys@marketboro.com>
2021-09-25 22:47:08 +0900

    HttpSecurity 필터추가 메소드 주석추가.

--- src/main/java/seol/study/jwtsample/common/jwt/JwtSecurityConfig.java
+++ src/main/java/seol/study/jwtsample/common/jwt/JwtSecurityConfig.java
@@ -16,6 +16,8 @@ public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurity
 	@Override
 	public void configure(HttpSecurity http) {
 		JwtFilter customFilter = new JwtFilter(tokenProvider);
+		// addFilterBefore: 지정된 필터 앞에 커스텀 필터를 추가 -> UsernamePasswordAuthenticationFilter 보다 먼저 filter 실행됨.
+		// addFilterAfter: 지정된 필터 뒤에 커스텀 필터를 추가 -> UsernamePasswordAuthenticationFilter 실행된 후에 filter 실행됨.
 		http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 }
